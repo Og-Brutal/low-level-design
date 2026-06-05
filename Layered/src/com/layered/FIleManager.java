28a8b3b720755cd8aeac3c8ad8bd3400f8b48bc7
Og-Brutal <ogbrutal2825@gmail.com>
2026-06-05 23:50:58 +0500

    Update FIleManager.java

--- Layered/src/com/layered/FIleManager.java
+++ Layered/src/com/layered/FIleManager.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
-
+  
 public class FIleManager {
 	
     public ArrayList<Double> getData(String rollNo) {
